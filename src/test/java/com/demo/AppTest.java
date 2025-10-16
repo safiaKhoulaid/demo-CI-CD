01d973a7cc5e453393e27dc6b35c951e62195e4b
safia-khoulaid <safiakhoulaid11@gmail.com>
2025-10-16 20:54:35 +0100

    commit all

---
+++ src/test/java/com/demo/AppTest.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void testAddition() {
+        assertEquals(5, com.example.App.add(2, 3));
+    }
+}
